05aa434729668fa013d6e93a6ffb9f5a5c8e6da6
Ramarraa <138695728+Ramarraa@users.noreply.github.com>
2026-09-19 17:39:21 +0300

    Delete fixed_uom_sale directory

--- fixed_uom_sale/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import (
-    account_move_line,
-    product_template,
-    purchase_order_line,
-    sale_order_line,
-)
--- fixed_uom_sale/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import test_tcf_product_defaults
